d5cc04c13921a79fd1e9d8d632c3b5f63dc6bf3c
Grechishnikov-Zarin Evgeniy <grecha-zarin@yandex.ru>
2023-06-18 23:45:59 +0300

    prepare to api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Едит 1211
+          Едит
           <code>src/App.js</code>
           and save to reload.
         </p>
